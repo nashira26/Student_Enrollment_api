87f098dff3546200bf92cb7c8d8b4956dd63ab34
nashira_26 <fathimanashira@gmail.com>
2023-07-05 12:50:30 +0530

    Add new files for building endpoints - Added views.py and built endpoint view for getting all funnel statuses - Added path for getting all funnel statuses in urls.py

--- apiproject/enrollment/views.py
+++ apiproject/enrollment/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
+from .models import FunnelStatus, Student, Log
+from .serializers import FunnelStatusSerializer, StudentSerializer, LogSerializer
 
-# Create your views here.
+#api endpoint for getting list of all the funnel status
+@api_view(['GET'])
+def get_funnel_status_list(request):
+
+    #get all status
+    #serialize them
+    #return json
+    statuses = FunnelStatus.objects.all()
+    serializer = FunnelStatusSerializer(statuses, many=True)
+    return JsonResponse({'statuses': serializer.data})
+
+
